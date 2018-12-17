866ccfb6d817a257f03df293fa25b753d57e4610
Nishant Thakur <nt.nishantt@gmail.com>
2018-12-17 17:32:43 -0600

    preprocessing of data

--- Classifiaction_using_CNN_tensorflow/main.py
+++ Classifiaction_using_CNN_tensorflow/main.py
@@ -8,3 +8,41 @@ def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
     labels = batch['labels']
         
     return features, labels
+
+
+def load_label_names():
+    return ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+batch_id = 3
+sample_id = 7000
+display_stats(cifar10_dataset_folder_path, batch_id, sample_id)
+
+
+def normalize(x):
+    """
+        argument
+            - x: input image data in numpy array [32, 32, 3]
+        return
+            - normalized x 
+    """
+    min_val = np.min(x)
+    max_val = np.max(x)
+    x = (x-min_val) / (max_val-min_val)
+    return x
+
+
+def one_hot_encode(x):
+    """
+        argument
+            - x: a list of labels
+        return
+            - one hot encoding matrix (number of labels, number of class)
+    """
+    encoded = np.zeros((len(x), 10))
+    
+    for idx, val in enumerate(x):
+        encoded[idx][val] = 1
+    
+    return encoded
+
